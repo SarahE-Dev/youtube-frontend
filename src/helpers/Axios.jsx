9b2054c02c72162452106ad6e1ed24d7ff0869b0
Sarah E <seatherly.prsvr@gmail.com>
2025-01-05 14:16:57 -0600

    axios instance

--- src/helpers/Axios.jsx
+++ src/helpers/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || "https://bluezack.adaptable.app/api/users",
+    baseURL: import.meta.env.VITE_BASE_URL || "https://me-plus-you-backend.onrender.com/api/users",
     withCredentials: true
 });
 
